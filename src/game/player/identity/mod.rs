8b0934c3d03d9e99a2dab75e264fba335af99bab
Jan Bataclan <jebataclan03@gmail.com>
2024-07-26 02:07:40 +0800

    Fix issue with player name having \r\n at the end

--- src/game/player/identity/mod.rs
+++ src/game/player/identity/mod.rs
@@ -20,13 +20,25 @@ impl Identity {
             let mut input = String::new();
         
             match io::stdin().read_line(&mut input) {
-                Ok(_) => return Identity::new(input),
+                Ok(_) => {
+                    trim_newline(&mut input);
+                    return Identity::new(input)
+                },
                 Err(err) => {
                     println!("Failed to read line with Error: {}", err);
                     input.clear();
                     continue;
                 },
             }
+        }
+    }
+}
+
+fn trim_newline(s: &mut String) {
+    if s.ends_with('\n') {
+        s.pop();
+        if s.ends_with('\r') {
+            s.pop();
         }
     }
 }
